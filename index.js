0abf91ce874a038c72b9a3ef603a474109db24b2
Amanda Wasinger <amandarm0928@outlook.com>
2022-05-02 21:01:17 -0500

    all the things

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 let content = document.querySelector('.js-generated.content')
+let header = document.createElement('h1')
+header.append('Rizzo')
+header.setAttribute('class', 'dog-name')
+content.append(header)
+
+let dogContent = document.createElement('div')
+dogContent.setAttribute('class', 'dog-content')
+content.append(dogContent)
+
+let dogImage = document.createElement('img')
+dogImage.setAttribute('class', 'dog-image')
+dogImage.setAttribute('src', './assets/rizzo.jpg')
+dogContent.append(dogImage)
+
+let dogDetails = document.createElement('div')
+dogDetails.setAttribute('class', 'dog-details')
+dogContent.append(dogDetails)
+
+let header1 = document.createElement('h3')
+header1.append('Description:')
+dogDetails.append(header1)
